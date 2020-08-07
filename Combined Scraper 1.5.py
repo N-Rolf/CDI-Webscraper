29ac0136ecc9a54f38abc44196d939574db636c5
blockitect <neilrolf24@gmail.com>
2020-08-07 15:28:08 -0500

    Update Combined Scraper 1.5.py

--- Combined Scraper 1.5.py
+++ Combined Scraper 1.5.py
@@ -1,9 +1,8 @@
-import requests
-import bs4
-import csv
-import urllib3
 import re
+import requests, bs4, webbrowser
 from bs4 import BeautifulSoup
+import tkinter as tk
+from tkinter import *
 
 #CITY OF BROOKINGS
 brookingsBaseUrl = 'https://cityofbrookings.org/'
@@ -12,29 +11,48 @@ brookingsBaseUrl = 'https://cityofbrookings.org/'
 brkngsUrl = 'https://cityofbrookings.org/Bids.aspx?CatID=All&txtSort=Category&showAllBids=&Status='
 brkngsPage = requests.get(brkngsUrl)
 
-#error checks
-#print(type(brkngsPage))
-#print(brkngsPage.status_code == requests.codes.ok)
-#print(len(brkngsPage.text))
-
 #beautifulSoup
 brkngsSoup = BeautifulSoup(brkngsPage.content, 'html5lib')
 brookingsActive = brkngsSoup.select('a[href*="bids.aspx"]')
 
-print("Brookings Open Projects:")
-brooklist = []
+brookTitles = []
+brookSites = []
 if len(brookingsActive) != 0:
 	for element in brookingsActive:
-		brooklist.append(element.text + ", " + (brookingsBaseUrl + element.get('href')))
-	
+		brookTitles.append(element.text)
+		brookSites.append(brookingsBaseUrl + element.get('href'))	
 		unwanted = [i for i, s in enumerate(brookingsActive) if 'Read\xa0on' in s]
 	for ele in sorted(unwanted, reverse = True):
-		del brooklist[ele]
-	print(*brooklist, sep = "\n")
+		del brookTitles[ele]
+		del brookSites[ele]
 else:
-	print('none')
+	brookTitles.append('none')
+	brookSites.append('none')
 
-print('\n')
+#console progress
+print("Brookings Done!" , len(brookTitles))
+#tkinter
+root = tk.Tk()
+#root.geometry("900x400")
+
+scroll_bar = Scrollbar(root, orient="vertical")
+scroll_bar.grid(row=0, column=3, sticky='ns')
+
+def openbrowser(val):
+	webbrowser.open(val)
+
+brookHeader = tk.Label(text="Brookings Open Projects")
+brookHeader.grid(row=0, column=0)
+
+for idx, val in enumerate(brookTitles):
+	siteLabels = val
+	siteLabels = tk.Label(text=val)
+	siteLabels.grid(row=(idx), column=1)
+
+for idx, val in enumerate(brookSites):
+		siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
+		siteButtons.grid(row=(idx), column=2)
+
 #CITY OF SIOUX FALLS
 sfbaseUrl = 'siouxfalls.org'
 
@@ -46,14 +64,32 @@ siouxfallsPage = requests.get(siouxfallsUrl)
 siouxfallsSoup = BeautifulSoup(siouxfallsPage.content, 'html5lib')
 sfactive = siouxfallsSoup.table.findAll("a")
 
-print("Sioux Falls Open Projects:")
+sfTitles = []
+sfSites = []
 if len(sfactive) != 0:
 	for element in sfactive:
-		print(element.text + ", " + (sfbaseUrl + element.get('href')))
+		sfTitles.append(element.text)
+		sfSites.append(sfbaseUrl + element.get('href'))
 else:
-	print('none')
+	sfTitles.append('none')
+	sfSites.append('none')
 
-print('\n')
+#console progress
+print("Sioux Falls Done!" , len(sfTitles))
+#ui counter
+sfCount = (len(brookTitles))
+#tkinter
+sfHeader = tk.Label(text="Sioux Falls Open Projects")
+sfHeader.grid(row=sfCount, column=0)
+
+for idx, val in enumerate(sfTitles):
+	siteLabels = tk.Label(text=val)
+	siteLabels.grid(row=(idx + sfCount), column=1)
+
+for idx, val in enumerate(sfSites):
+	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
+	siteButtons.grid(row=(idx + sfCount), column=2)
+
 #CITY OF WATERTOWN
 watertownBaseUrl = 'https://watertown.us'
 
@@ -65,20 +101,38 @@ watertownPage = requests.get(watertownUrl)
 watertownSoup = BeautifulSoup(watertownPage.content, 'html5lib')
 watertownActive = watertownSoup.select('a[href*="bids.aspx"]')
 
-print("Watertown Open Projects:")
-waterlist = []
+waterTitles = []
+waterSites = []
 if len(watertownActive) != 0:
 	for element in watertownActive:
-		waterlist.append(element.text + ", " + (watertownBaseUrl + element.get('href')))
-	
+		waterTitles.append(element.text)
+		waterSites.append(watertownBaseUrl + element.get('href'))
 		unwanted = [i for i, s in enumerate(watertownActive) if 'Read\xa0on' in s]
 	for ele in sorted(unwanted, reverse = True):
-		del waterlist[ele]
-	print(*waterlist, sep = "\n")
+		del waterTitles[ele]
+		del waterSites[ele]
 else:
-	print('none')
+	waterTitles.append('none')
+	waterSites.append('none')
 
-print('\n')
+#console progress
+print("Watertown Done!" , len(waterTitles))
+#ui counter
+waterCount = \
+(len(brookTitles) \
++ len(sfTitles))
+#tkinter
+waterHeader = tk.Label(text="Watertown Open Projects")
+waterHeader.grid(row=waterCount, column=0)
+
+for idx, val in enumerate(waterTitles):
+	siteLabels = tk.Label(text=val)
+	siteLabels.grid(row=(idx + waterCount), column=1)
+
+for idx, val in enumerate(waterSites):
+	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
+	siteButtons.grid(row=(idx + waterCount), column=2)
+
 #CITY OF ABERDEEN
 aberdeenBaseUrl = 'https://aberdeen.sd.us/'
 
@@ -90,20 +144,39 @@ aberdeenPage = requests.get(aberdeenUrl)
 aberdeenSoup = BeautifulSoup(aberdeenPage.content, 'html5lib')
 aberdeenActive = aberdeenSoup.select('a[href*="bids.aspx"]')
 
-print("Aberdeen Open Projects:")
-aberlist = []
+aberTitles = []
+aberSites = []
 if len(aberdeenActive) != 0:
 	for element in aberdeenActive:
-		aberlist.append(element.text + ", " + (aberdeenBaseUrl + element.get('href')))
-	
+		aberTitles.append(element.text)
+		aberSites.append(aberdeenBaseUrl + element.get('href'))
 		unwanted = [i for i, s in enumerate(aberdeenActive) if 'Read\xa0on' in s]
 	for ele in sorted(unwanted, reverse = True):
-		del aberlist[ele]
-	print(*aberlist, sep = "\n")
+		del aberTitles[ele]
+		del aberSites[ele]
 else:
-	print('none')
+	aberTitles.append('none')
+	aberSites.append('none')
 
-print('\n')
+#console progress
+print("Aberdeen Done!" , len(aberTitles))
+#ui counter
+aberCount = \
+(len(brookTitles) \
++ len(sfTitles) \
++ len(waterTitles))
+#tkinter
+aberHeader = tk.Label(text="Aberdeen Open Projects")
+aberHeader.grid(row=aberCount, column=0)
+
+for idx, val in enumerate(aberTitles):
+	siteLabels = tk.Label(text=val)
+	siteLabels.grid(row=(idx + aberCount), column=1)
+
+for idx, val in enumerate(aberSites):
+	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
+	siteButtons.grid(row=(idx + aberCount), column=2)
+
 #CITY OF HURON
 huronBaseUrl = 'https://huronsd.com'
 
@@ -115,23 +188,45 @@ huronPage = requests.get(huronUrl)
 huronSoup = BeautifulSoup(huronPage.content, 'html5lib')
 huronActive = huronSoup.select('a[href*="bids/view/id"]')
 
-huronlist = []
+huronTitles = []
+huronSites = []
+huronList = []	#likely empty return
 
-yearNumRegex = re.compile(r'\d{4}-\d{2}')
+yearNumRegex = re.compile(r'\d{4}-\d{2}') #wtf huron
 
-print("Huron Open Projects:")
 if len(huronActive) != 0:
 	for element in huronActive:
-		huronlist.append(element.text + ", " + (huronBaseUrl + element.get('href')))
-	
-		unwanted = [i for i, word in enumerate(huronlist) if re.search(yearNumRegex, word)] #idk why this works
+		huronList.append(element.text + ", " + huronBaseUrl + element.get('href'))
+		huronTitles.append(element.text)
+		huronSites.append(huronBaseUrl + element.get('href'))
+		unwanted = [i for i, word in enumerate(huronList) if re.search(yearNumRegex, word)] #idk why this works
 	for ele in sorted(unwanted, reverse = True):
-		del huronlist[ele]
-	print(*huronlist, sep = "\n")
+		del huronTitles[ele]
+		del huronSites[ele]
 else:
-	print('none')
+	huronTitles.append('none')
+	huronSites.append('none')
 
-print('\n')
+#console progress
+print("Huron Done!" , len(huronTitles))
+#ui counter
+huronCount = \
+(len(brookTitles) \
++ len(sfTitles) \
++ len(waterTitles) \
++ len(aberTitles))
+#tkinter
+huronHeader = tk.Label(text="Huron Open Projects")
+huronHeader.grid(row=huronCount, column=0)
+
+for idx, val in enumerate(huronTitles):
+	siteLabels = tk.Label(text=val)
+	siteLabels.grid(row=(idx + huronCount), column=1)
+
+for idx, val in enumerate(huronSites):
+	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
+	siteButtons.grid(row=(idx + huronCount), column=2)
+
 #CITY OF PIERRE
 pierreBaseUrl = 'https://cityofpierre.org/'
 
@@ -144,20 +239,42 @@ pierreSoup = BeautifulSoup(pierrePage.content, 'html5lib')
 
 pierreActive = pierreSoup.select('a[href*="bids.aspx"]')
 
-print("Pierre Open Projects:")
-pierreList = []
+pierreTitles = []
+pierreSites = []
+
 if len(pierreActive) != 0:
 	for element in pierreActive:
-		pierreList.append(element.text + ", " + (pierreBaseUrl + element.get('href')))
-	
+		pierreTitles.append(element.text)
+		pierreSites.append(pierreBaseUrl + element.get('href'))
 		unwanted = [i for i, s in enumerate(pierreActive) if 'Read\xa0on' in s]
 	for ele in sorted(unwanted, reverse = True):
-		del pierreList[ele]
-	print(*pierreList, sep = "\n")
+		del pierreTitles[ele]
+		del pierreSites[ele]
 else:
-	print('none')
+	pierreTitles.append('none')
+	pierreSites.append('none')
 
-print('\n')
+#console progress
+print("Peirre Done!" , len(pierreTitles))
+#ui counter
+pierreCount = \
+(len(brookTitles) \
++ len(sfTitles) \
++ len(waterTitles) \
++ len(aberTitles) \
++ len(huronTitles))
+#tkinter
+pierreHeader = tk.Label(text="Pierre Open Projects")
+pierreHeader.grid(row=pierreCount, column=0)
+
+for idx, val in enumerate(pierreTitles):
+	siteLabels = tk.Label(text=val)
+	siteLabels.grid(row=(idx + pierreCount), column=1)
+
+for idx, val in enumerate(pierreSites):
+	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
+	siteButtons.grid(row=(idx + pierreCount), column=2)
+
 #CITY OF MITCHELL
 mitchellBaseUrl = 'https://www.cityofmitchell.org/'
 
@@ -170,20 +287,42 @@ mitchellSoup = BeautifulSoup(mitchellPage.content, 'html5lib')
 
 mitchellActive = mitchellSoup.select('a[href*="bids.aspx"]')
 
-print("Mitchell Open Projects:")
-mitchellList = []
+mitchellTitles = []
+mitchellSites = []
 if len(mitchellActive) != 0:
 	for element in mitchellActive:
-		mitchellList.append(element.text + ", " + (mitchellBaseUrl + element.get('href')))
-	
+		mitchellTitles.append(element.text)
+		mitchellSites.append(mitchellBaseUrl + element.get('href'))
 		unwanted = [i for i, s in enumerate(mitchellActive) if 'Read\xa0on' in s]
 	for ele in sorted(unwanted, reverse = True):
-		del mitchellList[ele]
-	print(*mitchellList, sep = "\n")
+		del mitchellTitles[ele]
+		del mitchellSites[ele]
 else:
-	print('none')
+	mitchellTitles.append('none')
+	mitchellSites.append('none')
 
-print('\n')
+#console progress
+print("Mitchell Done!" , len(mitchellTitles))
+#ui counter
+mitchellCount = \
+(len(brookTitles) \
++ len(sfTitles) \
++ len(waterTitles) \
++ len(aberTitles) \
++ len(huronTitles) \
++ len(pierreTitles))
+#tkinter
+mitchellHeader = tk.Label(text="Mitchell Open Projects")
+mitchellHeader.grid(row=mitchellCount, column=0)
+
+for idx, val in enumerate(mitchellTitles):
+	siteLabels = tk.Label(text=val)
+	siteLabels.grid(row=(idx + mitchellCount), column=1)
+
+for idx, val in enumerate(mitchellSites):
+	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
+	siteButtons.grid(row=(idx + mitchellCount), column=2)
+
 #CITY OF YANKTON
 yanktonBaseUrl = 'https://www.cityofyankton.org/'
 
@@ -196,14 +335,39 @@ yanktonSoup = BeautifulSoup(yanktonPage.content, 'html5lib')
 
 yanktonActive = yanktonSoup.select('a[href*="Components/RFP"]')
 
-print("Yankton Open Projects:")
+yanktonTitles = []
+yanktonSites = []
 if len(yanktonActive) != 0:
 	for element in yanktonActive:
-		print(element.text + ", " + (yanktonBaseUrl + element.get('href')))
+		yanktonTitles.append(element.text)
+		yanktonSites.append(yanktonBaseUrl + element.get('href'))
 else:
-	print('none')
+	yanktonTitles.append('none')
+	yanktonSites.append('none')
 
-print('\n')
+#console progress
+print("Yankton Done!" , len(yanktonTitles))
+#ui counter
+yanktonCount = \
+(len(brookTitles) \
++ len(sfTitles) \
++ len(waterTitles) \
++ len(aberTitles) \
++ len(huronTitles) \
++ len(pierreTitles) \
++ len(mitchellTitles))
+#tkinter
+yanktonHeader = tk.Label(text="Yankton Open Projects")
+yanktonHeader.grid(row=yanktonCount, column=0)
+
+for idx, val in enumerate(yanktonTitles):
+	siteLabels = tk.Label(text=val)
+	siteLabels.grid(row=(idx + yanktonCount), column=1)
+
+for idx, val in enumerate(yanktonSites):
+	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
+	siteButtons.grid(row=(idx + yanktonCount), column=2)
+
 #CITY OF SPEARFISH
 spearfishBaseUrl = 'https://www.cityofspearfish.com/'
 
@@ -216,20 +380,44 @@ spearfishSoup = BeautifulSoup(spearfishPage.content, 'html5lib')
 
 spearfishActive = spearfishSoup.select('a[href*="bids.aspx"]')
 
-print("Spearfish Open Projects:")
-spearfishList = []
+spearfishTitles = []
+spearfishSites = []
 if len(spearfishActive) != 0:
 	for element in spearfishActive:
-		spearfishList.append(element.text + ", " + (spearfishBaseUrl + element.get('href')))
-	
+		spearfishTitles.append(element.text)
+		spearfishSites.append(spearfishBaseUrl + element.get('href'))
 		unwanted = [i for i, s in enumerate(spearfishActive) if 'Read\xa0on' in s]
 	for ele in sorted(unwanted, reverse = True):
-		del spearfishList[ele]
-	print(*spearfishList, sep = "\n")
+		del spearfishTitles[ele]
+		del spearfishSites[ele]
 else:
-	print('none')
+	spearfishTitles.append('none')
+	spearfishSites.append('none')
 
-print('\n')
+#console progress
+print("Spearfish Done!" , len(spearfishTitles))
+#ui counter
+spearfishCount = \
+(len(brookTitles) \
++ len(sfTitles) \
++ len(waterTitles) \
++ len(aberTitles) \
++ len(huronTitles) \
++ len(pierreTitles) \
++ len(mitchellTitles) \
++ len(yanktonTitles))
+#tkinter
+spearfishHeader = tk.Label(text="Spearfish Open Projects")
+spearfishHeader.grid(row=spearfishCount, column=0)
+
+for idx, val in enumerate(spearfishTitles):
+	siteLabels = tk.Label(text=val)
+	siteLabels.grid(row=(idx + spearfishCount), column=1)
+
+for idx, val in enumerate(spearfishSites):
+	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
+	siteButtons.grid(row=(idx + spearfishCount), column=2)
+
 #CITY OF VERMILLION
 vermillionBaseUrl = 'https://www.vermillion.us/'
 
@@ -242,20 +430,45 @@ vermillionSoup = BeautifulSoup(vermillionPage.content, 'html5lib')
 
 vermillionActive = vermillionSoup.select('a[href*="bids.aspx"]')
 
-print("Vermillion Open Projects:")
-vermillionList = []
+vermillionTitles = []
+vermillionSites = []
 if len(vermillionActive) != 0:
 	for element in vermillionActive:
-		vermillionList.append(element.text + ", " + (vermillionBaseUrl + element.get('href')))
-	
+		vermillionTitles.append(element.text)
+		vermillionSites.append(vermillionBaseUrl + element.get('href'))
 		unwanted = [i for i, s in enumerate(vermillionActive) if 'Read\xa0on' in s]
 	for ele in sorted(unwanted, reverse = True):
-		del vermillionList[ele]
-	print(*vermillionList, sep = "\n")
+		del vermillionTitles[ele]
+		del vermillionSites[ele]
 else:
-	print('none')
+	vermillionTitles.append('none')
+	vermillionSites.append('none')
 
-print('\n')
+#console progress
+print("Vermillion Done!" , len(vermillionTitles))
+#ui counter
+vermillionCount = \
+(len(brookTitles) \
++ len(sfTitles) \
++ len(waterTitles) \
++ len(aberTitles) \
++ len(huronTitles) \
++ len(pierreTitles) \
++ len(mitchellTitles) \
++ len(yanktonTitles) \
++ len(spearfishTitles))
+#tkinter
+vermillionHeader = tk.Label(text="Vermillion Open Projects")
+vermillionHeader.grid(row=vermillionCount, column=0)
+
+for idx, val in enumerate(vermillionTitles):
+	siteLabels = tk.Label(text=val)
+	siteLabels.grid(row=(idx + vermillionCount), column=1)
+
+for idx, val in enumerate(vermillionSites):
+	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
+	siteButtons.grid(row=(idx + vermillionCount), column=2)
+
 #CITY OF MADISON
 madisonBaseUrl = 'https://www.cityofmadisonsd.com/'
 
@@ -267,14 +480,43 @@ madisonPage = requests.get(madisonUrl)
 madisonSoup = BeautifulSoup(madisonPage.content, 'html5lib')
 madisonActive = madisonSoup.table.findAll("a")
 
-print("Madison Open Projects:")
+madisonTitles = []
+madisonSites = []
+
 if len(madisonActive) != 0:
 	for element in madisonActive:
-		print(element.text + ", " + (madisonBaseUrl + element.get('href')))
+		madisonTitles.append(element.text)
+		madisonSites.append(madisonBaseUrl + element.get('href'))
 else:
-	print('none')
+	madisonTitles.append('none')
+	madisonSites.append('none')
 
-print('\n')
+#console progress
+print("Madison Done!" , len(madisonTitles))
+#ui counter
+madisonCount = \
+(len(brookTitles) \
++ len(sfTitles) \
++ len(waterTitles) \
++ len(aberTitles) \
++ len(huronTitles) \
++ len(pierreTitles) \
++ len(mitchellTitles) \
++ len(yanktonTitles) \
++ len(spearfishTitles) \
++ len(vermillionTitles))
+#tkinter
+madisonHeader = tk.Label(text="Madison Open Projects")
+madisonHeader.grid(row=madisonCount, column=0)
+
+for idx, val in enumerate(madisonTitles):
+	siteLabels = tk.Label(text=val)
+	siteLabels.grid(row=(idx + madisonCount), column=1)
+
+for idx, val in enumerate(madisonSites):
+	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
+	siteButtons.grid(row=(idx + madisonCount), column=2)
+
 #South Dakota Game Fish & Parks
 sdgfpBaseUrl = 'https://gfp.sd.gov/'
 
@@ -286,19 +528,47 @@ sdgfpPage = requests.get(sdgfpUrl)
 sdgfpSoup = BeautifulSoup(sdgfpPage.content, 'html5lib')
 sdgfpActive = sdgfpSoup.table.findAll("a")
 
-print("South Dakota Game Fish & Parks Open Projects:")
-sdgfpList = []
+sdgfpTitles = []
+sdgfpSites = []
 if len(sdgfpActive) != 0:
 	for element in sdgfpActive:
-		sdgfpList.append(element.text + ", " + (sdgfpBaseUrl + element.get('href')))
-	
+		sdgfpTitles.append(element.text)
+		sdgfpSites.append(sdgfpBaseUrl + element.get('href'))
 		unwanted = [i for i, s in enumerate(sdgfpActive) if 'Yes' in s]
 	for ele in sorted(unwanted, reverse = True):
-		del sdgfpList[ele]
-	print(*sdgfpList, sep = "\n")
+		del sdgfpTitles[ele]
+		del sdgfpSites[ele]
 else:
-	print('none')
+	sdgfpTitles.append('none')
+	sdgfpSites.append('none')
 
-#window stays open
-input()
+#console progress
+print("SD Game Fish & Parks Done!" , len(sdgfpTitles))
+#ui counter
+sdgfpCount = \
+(len(brookTitles) \
++ len(sfTitles) \
++ len(waterTitles) \
++ len(aberTitles) \
++ len(huronTitles) \
++ len(pierreTitles) \
++ len(mitchellTitles) \
++ len(yanktonTitles) \
++ len(spearfishTitles) \
++ len(vermillionTitles) \
++ len(madisonTitles))
+#tkinter
+sdgfpHeader = tk.Label(text="SD Game Fish & Parks Open Projects")
+sdgfpHeader.grid(row=sdgfpCount, column=0)
+
+for idx, val in enumerate(sdgfpTitles):
+	siteLabels = tk.Label(text=val)
+	siteLabels.grid(row=(idx + sdgfpCount), column=1)
+
+for idx, val in enumerate(sdgfpSites):
+	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
+	siteButtons.grid(row=(idx + sdgfpCount), column=2)
+
+
+root.mainloop()
 
