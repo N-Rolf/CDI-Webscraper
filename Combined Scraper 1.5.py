dd3b8ae51f7fcfbec92e9c15fa17263eb313fab1
Ben Rolf <benrolf70@gmail.com>
2020-08-07 23:41:09 -0400

    fixed running count (#4)

--- Combined Scraper 1.5.py
+++ Combined Scraper 1.5.py
@@ -79,9 +79,8 @@ else:
 #console progress
 print("Sioux Falls Done!" , len(sfTitles))
 #ui counter
-sfCount = (len(brookTitles))
 runningcount = len(brookTitles)
-print("sf count",sfCount, "running count", runningcount)
+
 #tkinter
 sfHeader = tk.Label(text="Sioux Falls Open Projects")
 sfHeader.grid(row=runningcount, column=0)
@@ -122,12 +121,8 @@ else:
 #console progress
 print("Watertown Done!" , len(waterTitles))
 #ui counter
-waterCount = \
-(len(brookTitles) \
-+ len(sfTitles))
-
 runningcount += len(sfTitles)
-print("water count",waterCount,  "running count", runningcount)
+
 #tkinter
 waterHeader = tk.Label(text="Watertown Open Projects")
 waterHeader.grid(row=runningcount, column=0)
@@ -168,13 +163,8 @@ else:
 #console progress
 print("Aberdeen Done!" , len(aberTitles))
 #ui counter
-aberCount = \
-(len(brookTitles) \
-+ len(sfTitles) \
-+ len(waterTitles))
-
 runningcount += len(waterTitles)
-print("aber count",aberCount, "running count", runningcount)
+
 #tkinter
 aberHeader = tk.Label(text="Aberdeen Open Projects")
 aberHeader.grid(row=runningcount, column=0)
@@ -220,22 +210,19 @@ else:
 #console progress
 print("Huron Done!" , len(huronTitles))
 #ui counter
-huronCount = \
-(len(brookTitles) \
-+ len(sfTitles) \
-+ len(waterTitles) \
-+ len(aberTitles))
+runningcount += len(aberTitles)
+
 #tkinter
 huronHeader = tk.Label(text="Huron Open Projects")
-huronHeader.grid(row=huronCount, column=0)
+huronHeader.grid(row=runningcount, column=0)
 
 for idx, val in enumerate(huronTitles):
 	siteLabels = tk.Label(text=val)
-	siteLabels.grid(row=(idx + huronCount), column=1)
+	siteLabels.grid(row=(idx + runningcount), column=1)
 
 for idx, val in enumerate(huronSites):
 	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
-	siteButtons.grid(row=(idx + huronCount), column=2)
+	siteButtons.grid(row=(idx + runningcount), column=2)
 
 #CITY OF PIERRE
 pierreBaseUrl = 'https://cityofpierre.org/'
@@ -267,23 +254,19 @@ else:
 #console progress
 print("Peirre Done!" , len(pierreTitles))
 #ui counter
-pierreCount = \
-(len(brookTitles) \
-+ len(sfTitles) \
-+ len(waterTitles) \
-+ len(aberTitles) \
-+ len(huronTitles))
+runningcount += len(huronTitles)
+
 #tkinter
 pierreHeader = tk.Label(text="Pierre Open Projects")
-pierreHeader.grid(row=pierreCount, column=0)
+pierreHeader.grid(row=runningcount, column=0)
 
 for idx, val in enumerate(pierreTitles):
 	siteLabels = tk.Label(text=val)
-	siteLabels.grid(row=(idx + pierreCount), column=1)
+	siteLabels.grid(row=(idx + runningcount), column=1)
 
 for idx, val in enumerate(pierreSites):
 	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
-	siteButtons.grid(row=(idx + pierreCount), column=2)
+	siteButtons.grid(row=(idx + runningcount), column=2)
 
 #CITY OF MITCHELL
 mitchellBaseUrl = 'https://www.cityofmitchell.org/'
@@ -314,24 +297,19 @@ else:
 #console progress
 print("Mitchell Done!" , len(mitchellTitles))
 #ui counter
-mitchellCount = \
-(len(brookTitles) \
-+ len(sfTitles) \
-+ len(waterTitles) \
-+ len(aberTitles) \
-+ len(huronTitles) \
-+ len(pierreTitles))
+runningcount += len(pierreTitles)
+
 #tkinter
 mitchellHeader = tk.Label(text="Mitchell Open Projects")
-mitchellHeader.grid(row=mitchellCount, column=0)
+mitchellHeader.grid(row=runningcount, column=0)
 
 for idx, val in enumerate(mitchellTitles):
 	siteLabels = tk.Label(text=val)
-	siteLabels.grid(row=(idx + mitchellCount), column=1)
+	siteLabels.grid(row=(idx + runningcount), column=1)
 
 for idx, val in enumerate(mitchellSites):
 	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
-	siteButtons.grid(row=(idx + mitchellCount), column=2)
+	siteButtons.grid(row=(idx + runningcount), column=2)
 
 #CITY OF YANKTON
 yanktonBaseUrl = 'https://www.cityofyankton.org/'
@@ -358,25 +336,19 @@ else:
 #console progress
 print("Yankton Done!" , len(yanktonTitles))
 #ui counter
-yanktonCount = \
-(len(brookTitles) \
-+ len(sfTitles) \
-+ len(waterTitles) \
-+ len(aberTitles) \
-+ len(huronTitles) \
-+ len(pierreTitles) \
-+ len(mitchellTitles))
+runningcount += len(mitchellTitles)
+
 #tkinter
 yanktonHeader = tk.Label(text="Yankton Open Projects")
-yanktonHeader.grid(row=yanktonCount, column=0)
+yanktonHeader.grid(row=runningcount, column=0)
 
 for idx, val in enumerate(yanktonTitles):
 	siteLabels = tk.Label(text=val)
-	siteLabels.grid(row=(idx + yanktonCount), column=1)
+	siteLabels.grid(row=(idx + runningcount), column=1)
 
 for idx, val in enumerate(yanktonSites):
 	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
-	siteButtons.grid(row=(idx + yanktonCount), column=2)
+	siteButtons.grid(row=(idx + runningcount), column=2)
 
 #CITY OF SPEARFISH
 spearfishBaseUrl = 'https://www.cityofspearfish.com/'
@@ -407,26 +379,19 @@ else:
 #console progress
 print("Spearfish Done!" , len(spearfishTitles))
 #ui counter
-spearfishCount = \
-(len(brookTitles) \
-+ len(sfTitles) \
-+ len(waterTitles) \
-+ len(aberTitles) \
-+ len(huronTitles) \
-+ len(pierreTitles) \
-+ len(mitchellTitles) \
-+ len(yanktonTitles))
+runningcount += len(yanktonTitles)
+
 #tkinter
 spearfishHeader = tk.Label(text="Spearfish Open Projects")
-spearfishHeader.grid(row=spearfishCount, column=0)
+spearfishHeader.grid(row=runningcount, column=0)
 
 for idx, val in enumerate(spearfishTitles):
 	siteLabels = tk.Label(text=val)
-	siteLabels.grid(row=(idx + spearfishCount), column=1)
+	siteLabels.grid(row=(idx + runningcount), column=1)
 
 for idx, val in enumerate(spearfishSites):
 	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
-	siteButtons.grid(row=(idx + spearfishCount), column=2)
+	siteButtons.grid(row=(idx + runningcount), column=2)
 
 #CITY OF VERMILLION
 vermillionBaseUrl = 'https://www.vermillion.us/'
@@ -457,27 +422,19 @@ else:
 #console progress
 print("Vermillion Done!" , len(vermillionTitles))
 #ui counter
-vermillionCount = \
-(len(brookTitles) \
-+ len(sfTitles) \
-+ len(waterTitles) \
-+ len(aberTitles) \
-+ len(huronTitles) \
-+ len(pierreTitles) \
-+ len(mitchellTitles) \
-+ len(yanktonTitles) \
-+ len(spearfishTitles))
+runningcount += len(spearfishTitles)
+
 #tkinter
 vermillionHeader = tk.Label(text="Vermillion Open Projects")
-vermillionHeader.grid(row=vermillionCount, column=0)
+vermillionHeader.grid(row=runningcount, column=0)
 
 for idx, val in enumerate(vermillionTitles):
 	siteLabels = tk.Label(text=val)
-	siteLabels.grid(row=(idx + vermillionCount), column=1)
+	siteLabels.grid(row=(idx + runningcount), column=1)
 
 for idx, val in enumerate(vermillionSites):
 	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
-	siteButtons.grid(row=(idx + vermillionCount), column=2)
+	siteButtons.grid(row=(idx + runningcount), column=2)
 
 #CITY OF MADISON
 madisonBaseUrl = 'https://www.cityofmadisonsd.com/'
@@ -504,28 +461,19 @@ else:
 #console progress
 print("Madison Done!" , len(madisonTitles))
 #ui counter
-madisonCount = \
-(len(brookTitles) \
-+ len(sfTitles) \
-+ len(waterTitles) \
-+ len(aberTitles) \
-+ len(huronTitles) \
-+ len(pierreTitles) \
-+ len(mitchellTitles) \
-+ len(yanktonTitles) \
-+ len(spearfishTitles) \
-+ len(vermillionTitles))
+runningcount += len(vermillionTitles)
+
 #tkinter
 madisonHeader = tk.Label(text="Madison Open Projects")
-madisonHeader.grid(row=madisonCount, column=0)
+madisonHeader.grid(row=runningcount, column=0)
 
 for idx, val in enumerate(madisonTitles):
 	siteLabels = tk.Label(text=val)
-	siteLabels.grid(row=(idx + madisonCount), column=1)
+	siteLabels.grid(row=(idx + runningcount), column=1)
 
 for idx, val in enumerate(madisonSites):
 	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
-	siteButtons.grid(row=(idx + madisonCount), column=2)
+	siteButtons.grid(row=(idx + runningcount), column=2)
 
 #South Dakota Game Fish & Parks
 sdgfpBaseUrl = 'https://gfp.sd.gov/'
@@ -555,29 +503,19 @@ else:
 #console progress
 print("SD Game Fish & Parks Done!" , len(sdgfpTitles))
 #ui counter
-sdgfpCount = \
-(len(brookTitles) \
-+ len(sfTitles) \
-+ len(waterTitles) \
-+ len(aberTitles) \
-+ len(huronTitles) \
-+ len(pierreTitles) \
-+ len(mitchellTitles) \
-+ len(yanktonTitles) \
-+ len(spearfishTitles) \
-+ len(vermillionTitles) \
-+ len(madisonTitles))
+runningcount += len(madisonTitles)
+
 #tkinter
 sdgfpHeader = tk.Label(text="SD Game Fish & Parks Open Projects")
-sdgfpHeader.grid(row=sdgfpCount, column=0)
+sdgfpHeader.grid(row=runningcount, column=0)
 
 for idx, val in enumerate(sdgfpTitles):
 	siteLabels = tk.Label(text=val)
-	siteLabels.grid(row=(idx + sdgfpCount), column=1)
+	siteLabels.grid(row=(idx + runningcount), column=1)
 
 for idx, val in enumerate(sdgfpSites):
 	siteButtons = tk.Button(text=val, command=lambda x=val: openbrowser(x))
-	siteButtons.grid(row=(idx + sdgfpCount), column=2)
+	siteButtons.grid(row=(idx + runningcount), column=2)
 
 
 root.mainloop()
