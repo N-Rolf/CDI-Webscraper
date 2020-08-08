34d87235df4ef76701b6fb98512c586903c9e74d
magicbycalvin <magicbycalvin@gmail.com>
2020-08-08 10:21:31 -0500

    Initial architecture overhaul.
    Added a Python .gitignore file. Created four main modules. Began working on the webscraper module to avoid making the same calls several times.

---
+++ gui.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  8 09:26:42 2020
+
+@author: Unknown
+"""
+
---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  8 09:26:26 2020
+
+@author: Unknown
+"""
+
