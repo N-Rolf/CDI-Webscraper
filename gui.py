e89ec446d19e96dc33dcac454491bee9d71c1429
magicbycalvin <magicbycalvin@gmail.com>
2020-08-15 09:30:32 -0500

    Initial tkinter framework

--- gui.py
+++ gui.py
@@ -5,3 +5,16 @@ Created on Sat Aug  8 09:26:42 2020
 @author: Unknown
 """
 
+import tkinter as tk
+
+class MainApplication(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+
+        <create the rest of your GUI here>
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    MainApplication(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
