a56083d281db8e739e8f9fbb831e0b1e39ab7e44
magicbycalvin <magicbycalvin@gmail.com>
2020-08-15 10:15:12 -0500

    Added a navbar to the GUI.

--- gui.py
+++ gui.py
@@ -7,12 +7,36 @@ Created on Sat Aug  8 09:26:42 2020
 
 import tkinter as tk
 
-class MainApplication(tk.Frame):
+
+def donothing():
+    pass
+
+
+class Navbar(tk.Menu):
     def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent, *args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.parent = parent
 
-        <create the rest of your GUI here>
+        file = tk.Menu(self, tearoff=0)
+        file.add_command(label="New", command=donothing)
+        file.add_command(label="Open", command=donothing)
+        file.add_command(label="Save", command=donothing)
+        file.add_command(label='Read CSV', command=lambda: print('reading CSV...'))
+        file.add_separator()
+        file.add_command(label="Exit", command=lambda: print('Not actually quitting'))
+
+        self.add_cascade(label="File", menu=file)
+
+
+class MainApplication(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.parent = parent
+
+        self.navbar = Navbar(self)
+
+        self.parent.config(menu=self.navbar)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
